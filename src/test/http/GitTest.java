dc29d6f3c4e92bd6fd9a110da562c08416037577
drakart <57429706+drakart@users.noreply.github.com>
2021-09-15 15:11:43 +0900

    conflict 이차테스트

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -2,7 +2,7 @@ package test.http;
 
 public class GitTest {
 
-	private static final String DESC = "1 GIT TEST용 클래스 입니다.";
+	private static final String DESC = "1  GIT TEST용 클래스 입니다.";
 	private static final String CREDENTIAL = "GIT 인증 테스트";
 	private static final String GITHUB = "3 원격리포지토리에서 직접 수정";
 	private static final String LOCALCONFLICT = "Conflict 테스트";
